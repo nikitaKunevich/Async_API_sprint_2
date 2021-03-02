b6bfc2933dc4ce17da3c6cb5c3ce634f5c3afa44
Platon Dmitriev <platon.dmitriev@playgendary.com>
2021-03-03 00:14:59 +0300

    Add new person tests, handle more test cases

--- tests/functional/src/test_person.py
+++ tests/functional/src/test_person.py
@@ -89,47 +89,64 @@ async def person_movies(es_client: AsyncElasticsearch):
 
 
 @pytest.mark.asyncio
-async def test_person_search_single_match(session: aiohttp.ClientSession, es_client: AsyncElasticsearch,
-                                          persons: List):
-    # Выполнение запроса
+async def test_person_list(session: aiohttp.ClientSession, es_client: AsyncElasticsearch,
+                           persons: List):
+    response = await make_get_request(session, '/person')
+    assert response.status == 200
+    assert len(response.body) == 3
+
+
+@pytest.mark.asyncio
+async def test_person_search(session: aiohttp.ClientSession, es_client: AsyncElasticsearch,
+                             persons: List):
+    # Единственное совпадение
     response = await make_get_request(session, '/person', {'query': 'Chris'})
-    # Проверка результата
     assert response.status == 200
     assert len(response.body) == 1
     assert response.body[0]['uuid'] == '0040371d-f875-4d42-ab17-ffaf3cacfb91'
     assert response.body[0]['full_name'] == 'Chris Cooper'
     assert response.body[0]['roles'] == ["actor"]
     assert response.body[0]['film_ids'] == ["93d538fe-1328-4b4c-a327-f61a80f25a3c"]
-    # assert response.body == expected
+
+    # Несколько совпадений
+    response = await make_get_request(session, '/person', {'query': 'June'})
+    assert response.status == 200
+    assert len(response.body) == 2
+    expected_person_ids = [persons[1]['id'], persons[2]['id']]
+    assert response.body[0]['uuid'] in expected_person_ids
+    assert response.body[1]['uuid'] in expected_person_ids
+
+    # Лучшее совпадение
+    response = await make_get_request(session, '/person', {'query': 'June Laverick2'})
+    assert response.status == 200
+    assert len(response.body) == 2
+    assert response.body[0]['uuid'] == '0040371d-f875-4d42-ab17-ffaf3cacfb92'
+    assert response.body[0]['full_name'] == 'June Laverick2'
 
 
 @pytest.mark.asyncio
-async def test_person_search_multiple_match(session: aiohttp.ClientSession, es_client: AsyncElasticsearch,
-                                            persons: List):
-    # Выполнение запроса
-    response = await make_get_request(session, '/person', {'query': 'June'})
-    # Проверка результата
+async def test_person_search_paginated_and_limited_result(session: aiohttp.ClientSession,
+                                                          es_client: AsyncElasticsearch,
+                                                          persons: List):
+    response = await make_get_request(session, '/person', {'page[size]': 2})
     assert response.status == 200
     assert len(response.body) == 2
-    expected_ids = [persons[1]['id'], persons[2]['id']]
-    assert response.body[0]['uuid'] in expected_ids
-    assert response.body[1]['uuid'] in expected_ids
-    # assert response.body == expected
+
+    response = await make_get_request(session, '/person', {'page[size]': 2, 'page[number]': 2})
+    assert response.status == 200
+    assert len(response.body) == 1
 
 
 @pytest.mark.asyncio
 async def test_person_search_not_found(session: aiohttp.ClientSession, es_client: AsyncElasticsearch, persons: List):
-    # Выполнение запроса
     response = await make_get_request(session, '/person', {'query': 'Something'})
-    # Проверка результата
     assert response.status == 404
-    # assert response.body == expected
 
 
 @pytest.mark.asyncio
 async def test_person_detail(session: aiohttp.ClientSession, es_client: AsyncElasticsearch, persons: List):
-    looked_person_id = persons[0]['id']
-    person_detail_endpoint = f'/person/{looked_person_id}'
+    lookup_person_id = persons[0]['id']
+    person_detail_endpoint = f'/person/{lookup_person_id}'
     response = await make_get_request(session, person_detail_endpoint)
 
     assert response.status == 200
@@ -137,7 +154,6 @@ async def test_person_detail(session: aiohttp.ClientSession, es_client: AsyncEla
     assert response.body['full_name'] == 'Chris Cooper'
     assert response.body['roles'] == ["actor"]
     assert response.body['film_ids'] == ["93d538fe-1328-4b4c-a327-f61a80f25a3c"]
-    # assert response.body == expected
 
 
 @pytest.mark.asyncio
@@ -149,10 +165,11 @@ async def test_person_detail_not_found(session: aiohttp.ClientSession, es_client
 
 
 @pytest.mark.asyncio
-async def test_person_films_single_match(session: aiohttp.ClientSession, es_client: AsyncElasticsearch,
-                                         person_movies: List, persons: List):
-    looked_person_id = persons[0]['id']
-    person_film_endpoint = f'/person/{looked_person_id}/film'
+async def test_person_films(session: aiohttp.ClientSession, es_client: AsyncElasticsearch,
+                            person_movies: List, persons: List):
+    # Один фильм
+    lookup_person_id = persons[0]['id']
+    person_film_endpoint = f'/person/{lookup_person_id}/film'
     response = await make_get_request(session, person_film_endpoint)
 
     assert response.status == 200
@@ -160,12 +177,9 @@ async def test_person_films_single_match(session: aiohttp.ClientSession, es_clie
     assert response.body[0]['uuid'] == "93d538fe-1328-4b4c-a327-f61a80f25a3c"
     assert response.body[0]['title'] == 'Test Movie'
 
-
-@pytest.mark.asyncio
-async def test_person_films_multiple_match(session: aiohttp.ClientSession, es_client: AsyncElasticsearch,
-                                           person_movies: List, persons: List):
-    looked_person_id = persons[1]['id']
-    person_film_endpoint = f'/person/{looked_person_id}/film'
+    # Несколько фильмов
+    lookup_person_id = persons[1]['id']
+    person_film_endpoint = f'/person/{lookup_person_id}/film'
     response = await make_get_request(session, person_film_endpoint)
 
     expected_film_ids = [person_movies[0]['id'], person_movies[1]['id']]
@@ -174,3 +188,10 @@ async def test_person_films_multiple_match(session: aiohttp.ClientSession, es_cl
     assert len(response.body) == 2
     assert response.body[0]['uuid'] in expected_film_ids
     assert response.body[1]['uuid'] in expected_film_ids
+
+    # Ни одного фильма
+    lookup_person_id = persons[2]['id']
+    person_film_endpoint = f'/person/{lookup_person_id}/film'
+    response = await make_get_request(session, person_film_endpoint)
+
+    assert response.status == 404
