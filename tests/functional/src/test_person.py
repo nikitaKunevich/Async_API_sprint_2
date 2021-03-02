22dd97f3df7efedc8a9d55995ad0b3e4f3154931
Platon Dmitriev <platon.dmitriev@playgendary.com>
2021-03-02 23:33:17 +0300

    Refactor test person to use utils and conftest

--- tests/functional/src/test_person.py
+++ tests/functional/src/test_person.py
@@ -1,81 +1,17 @@
 import asyncio
-import json
 from typing import List
 
 import aiohttp
-import aioredis
 import pytest
 
-from dataclasses import dataclass
-
-from aioredis import Redis
-from multidict import CIMultiDictProxy
 from elasticsearch import AsyncElasticsearch
 
 from db.models import Film, Person
 from settings import Settings
 
+from utils import make_get_request
+
 settings = Settings()
-
-
-@dataclass
-class HTTPResponse:
-    body: dict
-    headers: CIMultiDictProxy[str]
-    status: int
-
-
-@pytest.fixture(scope='session')
-def event_loop():
-    loop = asyncio.get_event_loop()
-    yield loop
-    loop.close()
-
-
-@pytest.fixture(scope='session')
-async def redis_flush():
-    redis = await aioredis.create_redis_pool((settings.REDIS_HOST, settings.REDIS_PORT), minsize=10, maxsize=20)
-    await redis.flushall()
-    yield
-
-
-@pytest.fixture(scope='session')
-async def es_client():
-    client = AsyncElasticsearch(hosts=settings.es_url)
-    try:
-        await client.indices.delete('movies')
-        await client.indices.delete('genres')
-        await client.indices.delete('persons')
-    except:
-        pass
-
-    await client.indices.create('movies', body=json.load(open('/schemas/es.movies.schema.json')))
-    await client.indices.create('genres', body=json.load(open('/schemas/es.genres.schema.json')))
-    await client.indices.create('persons', body=json.load(open('/schemas/es.persons.schema.json')))
-
-    yield client
-    await client.indices.delete('movies')
-    await client.indices.delete('genres')
-    await client.indices.delete('persons')
-    await client.close()
-
-
-@pytest.fixture(scope='session')
-async def session():
-    session = aiohttp.ClientSession()
-    yield session
-    await session.close()
-
-
-async def make_get_request(session, method: str, params: dict = None) -> HTTPResponse:
-    params = params or {}
-    url = settings.api_host + '/v1' + method  # в боевых системах старайтесь так не делать!
-    async with session.get(url, params=params) as response:
-        return HTTPResponse(
-            body=await response.json(),
-            headers=response.headers,
-            status=response.status,
-        )
 
 
 @pytest.fixture
