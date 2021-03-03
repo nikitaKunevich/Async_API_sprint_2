a2201a27eb7b67111195f03c2206abf25b40bb24
Nikita Meshaninov <nm@decentury.io>
2021-03-03 16:16:09 +0700

    Update movies tests

--- tests/functional/conftest.py
+++ tests/functional/conftest.py
@@ -1,14 +1,17 @@
-from dataclasses import dataclass
-import json
-from pathlib import Path
-import aiohttp
-from aioredis.commands import Redis
-from elasticsearch import AsyncElasticsearch
-import pytest
 import asyncio
+import json
+from dataclasses import dataclass
+from pathlib import Path
+
+import aiohttp
 import aioredis
-from .settings import Settings
+import pytest
+from aioredis.commands import Redis
+from db.models import Film
+from elasticsearch import AsyncElasticsearch
 from multidict import CIMultiDictProxy
+
+from .settings import Settings
 
 
 @pytest.fixture(scope='session')
@@ -34,7 +37,7 @@ async def redis(settings) -> Redis:
 
 @pytest.fixture(scope='session')
 async def es_client(settings):
-    async def create_indexes(indx_name, schema_path):
+    async def create_indices(indx_name, schema_path):
         return await client.indices.create(indx_name, body=json.load(open(schema_path)))
 
     async def delete_indices(indx_name):
@@ -50,7 +53,7 @@ async def es_client(settings):
     except:
         pass
 
-    await asyncio.gather(*[create_indexes(index, get_schema_path(index)) for index in index_names])
+    await asyncio.gather(*[create_indices(index, get_schema_path(index)) for index in index_names])
 
     yield client
 
@@ -85,4 +88,199 @@ def make_get_request(session, settings):
                 headers=response.headers,
                 status=response.status,
             )
+    return inner
+
+
+@pytest.fixture
+async def films(es_client):
+    # Заполнение данных для теста
+
+    # 2 фильма Star Wars, 2 фильма Star Trek
+    test_films = [
+        {
+            "imdb_rating": 8.1,
+            "id": "fff4777b-7fe5-4be9-a6ea-6acb75b96fb0",
+            "title": "Star Wars: The Old Republic - Rise of the Hutt Cartel",
+            "description": "With the Sith emperor defeated, the republic & empire grow ever more desperate to win the conflict. All eyes turn on the planet Makeb, where a mysterious substance called isotope-5 has been discovered. This substance has the power to fuel a massive army for whoever wields it. Unfortunately for both sides, the hutt cartel has control of the planet, and will not give it up lightly...",
+            "actors_names": ["a", "aa"],
+            "writers_names": ["w", "ww"],
+            "directors_names": ["d"],
+            "genres_names": ["Action"],
+            "actors": [
+                {
+                    "id": "237f5326-c9b6-43c9-9a1a-80cd21088e20",
+                    "name": "a"
+                },
+                {
+                    "id": "1a9ca60c-affe-4e69-aa03-9d07d1b977c1",
+                    "name": "aa"
+                }
+            ],
+            "writers": [
+                {
+                    "id": "279842e9-28f3-49f4-9914-46f8b4ad92c9",
+                    "name": "w"
+                },
+                {
+                    "id": "503db68c-d0f8-468a-a4f2-d6f87b81afcc",
+                    "name": "ww"
+                }
+            ],
+            "directors": [
+                {
+                    "id": "f87486ec-7682-407b-8346-23947d944820",
+                    "name": "d"
+                }
+            ],
+            "genres": [
+                {
+                    "id": "6ae86dab-dbc2-478c-9151-ddfd8c92c10a",
+                    "name": "Action"
+                }
+            ]
+        },
+        {
+            "imdb_rating": 7.1,
+            "id": "fdaf4660-6456-4e99-8009-c9e0db67ea90",
+            "title": "Star Wars: The New Republic Anthology",
+            "description": "Boba Fett has been trapped for 30 years in the Great Pit of Carkoon; makes his escape and is now allies with the Rebellion.",
+            "actors_names": ["a2", "aa2"],
+            "writers_names": ["w2", "ww2"],
+            "directors_names": ["d2", "dd2"],
+            "genres_names": ["Thriller"],
+            "actors": [
+                {
+                    "id": "929d9e18-f5d1-4198-97b2-4e2ddbe28f76",
+                    "name": "a2"
+                },
+                {
+                    "id": "620a94fe-f76e-4a0e-b6c9-f8f7c14d811d",
+                    "name": "aa2"
+                }
+            ],
+            "writers": [
+                {
+                    "id": "845de5a7-b57f-42a2-a2d1-ad07d88e1689",
+                    "name": "w2"
+                },
+                {
+                    "id": "bb871757-9e79-4bd6-978c-a683ae0c127c",
+                    "name": "ww2"
+                }
+            ],
+            "directors": [
+                {
+                    "id": "c038eeb8-529f-4c50-877f-3d0af97bee9f",
+                    "name": "d2"
+                },
+                {
+                    "id": "8a2b3048-d9b7-4874-9ef5-cb9155b9fc38",
+                    "name": "dd2"
+                }
+            ],
+            "genres": [
+                {
+                    "id": "9a09f1b3-a3f8-4939-b175-29beeba1a6b3",
+                    "name": "Thriller"
+                }
+            ]
+        },
+        {
+            "imdb_rating": 5.7,
+            "id": "fdfc8350-4ec4-45c5-9ce9-9139c3e2fce6",
+            "title": "Star Trek: Horizon",
+            "description": "In a time prior to the United Federation of Planets, a young coalition of worlds led by Earth battle the Romulan Star Empire for their very survival.",
+            "actors_names": ["a3", "aa3"],
+            "writers_names": ["w3", "ww3"],
+            "directors_names": ["d3"],
+            "genres_names": ["War"],
+            "actors": [
+                {
+                    "id": "b29a3239-800d-4c6c-9f3f-3280f503566e",
+                    "name": "a3"
+                },
+                {
+                    "id": "84df81ae-7845-4d78-963e-47b0a8a4b972",
+                    "name": "aa3"
+                }
+            ],
+            "writers": [
+                {
+                    "id": "454cbe05-efcd-4f17-91ae-3c6b670e3e76",
+                    "name": "w3"
+                },
+                {
+                    "id": "2ee03ec0-ea75-42f5-841e-189ad7aadd72",
+                    "name": "ww3"
+                }
+            ],
+            "directors": [
+                {
+                    "id": "45cbadba-a15d-4036-87e8-a64e749f0ad8",
+                    "name": "d3"
+                }
+            ],
+            "genres": [
+                {
+                    "id": "67ae3870-b50d-4508-b2a6-ade667149ceb",
+                    "name": "War"
+                }
+            ]
+        },
+        {
+            "imdb_rating": 7.5,
+            "id": "fd7d8ea7-396a-42af-8b0a-ec2747e27506",
+            "title": "Star Trek: Enterprise",
+            "description": "The year is 2151. Earth has spent the last 88 years since learning how to travel faster than the speed of light studying under the wisdom of their alien ally called the 'Vulcans'. Now, the first crew of human explorers sets out into deep space on a ship called the 'Enterprise' to see what is beyond our solar system.",
+            "actors_names": ["a4", "aa4"],
+            "writers_names": ["w4", "ww4"],
+            "directors_names": ["d4"],
+            "genres_names": ["Mystery"],
+            "actors": [
+                {
+                    "id": "80227dc8-3e00-4b41-a2e3-350baacd9acb",
+                    "name": "a4"
+                },
+                {
+                    "id": "914732e3-8616-4c68-860f-64f42f78754d",
+                    "name": "aa4"
+                }
+            ],
+            "writers": [
+                {
+                    "id": "449b2581-7459-4ab4-b504-ef9b6244a816",
+                    "name": "w4"
+                },
+                {
+                    "id": "d4017eca-1417-4bd4-8414-2ca2686bf94b",
+                    "name": "ww4"
+                }
+            ],
+            "directors": [
+                {
+                    "id": "b2a9534a-2f6b-4754-9bd2-be7ce2233b51",
+                    "name": "d4"
+                }
+            ],
+            "genres": [
+                {
+                    "id": "e86e1386-64d4-4e0c-9162-af3363082727",
+                    "name": "Mystery"
+                }
+            ]
+        }
+    ]
+
+    films_obj = [Film.parse_obj(film) for film in test_films]
+    await asyncio.gather(*[es_client.index('movies', body=film.dict(), id=film.id, refresh='wait_for') for film in films_obj])
+
+    return films_obj
+
+
+@pytest.fixture
+@pytest.mark.asyncio
+async def make_group_get_request(make_get_request):
+    async def inner(urls: list) -> list[HTTPResponse]:
+        requests = [make_get_request(url) for url in urls]
+        return await asyncio.gather(*requests)
     return inner
--- tests/functional/src/test_movies.py
+++ tests/functional/src/test_movies.py
@@ -1,87 +1,159 @@
-from elasticsearch import AsyncElasticsearch
+from aioredis import Redis
 import pytest
-import asyncio
-
-from db.models import Film
-
-
-@pytest.fixture
-async def films(es_client):
-    # Заполнение данных для теста
-    films = [{
-        'id': '08293f6d-90e4-467d-b2c3-ee85b5b0d326',
-        'title': 'Night Owl',
-        'actors_names': [],
-        'writers_names': [],
-        'directors_names': [],
-        'genres_names': [],
-        'actors': [],
-        'writers': [],
-        'directors': [],
-        'genres': [],
-    }, {
-        'id': 'd3a8967d-9601-4b96-a43c-6edfc0dd372b',
-        'title': 'Star was Born',
-        'actors_names': [],
-        'writers_names': [],
-        'directors_names': [],
-        'genres_names': [],
-        'actors': [],
-        'writers': [],
-        'directors': [],
-        'genres': [],
-    }]
-
-    await asyncio.gather(*[es_client.index('movies', body=Film.parse_obj(film).dict(), id=film['id'], refresh='wait_for') for film in films])
-
-    return films
+import urllib
 
 
 @pytest.mark.asyncio
-async def test_search(es_client: AsyncElasticsearch, make_get_request, films):
-    # Проверка находится ли внутри elastic
-    assert await es_client.get(id=films[0]['id'], index='movies')
+async def test_single_search(make_group_get_request, films):
+    query_str = '/film/?query='
+    response_film_star_wars, response_film_star_trek = await make_group_get_request([
+        query_str + urllib.parse.quote('Hutt Cartel'),
+        query_str + urllib.parse.quote('Horizon')
+    ])
 
-    # Выполнение запроса
-    requests = [
-        make_get_request('/film/?query=Star'),               # Удачный запрос 1
-        make_get_request('/film/?query=Night%20Owl'),        # Удачный запрос 2
-        make_get_request('/film/?query=SomethingNotFound'),  # Неудачный запрос 3
-    ]
+    assert response_film_star_wars.status == 200
 
-    r1, r2, r3 = await asyncio.gather(*requests)
+    assert len(response_film_star_wars.body) == 1
+    film_star_wars_es = response_film_star_wars.body[0]
+    film_star_wars = films[0]
+    assert film_star_wars_es['title'] == film_star_wars.title
+    assert film_star_wars_es['uuid'] == film_star_wars.id
+    assert film_star_wars_es['imdb_rating'] == film_star_wars.imdb_rating
 
-    # 1
-    assert r1.status == 200
-    assert len(r1.body) == 1
-    assert r1.body[0]['uuid'] == films[1]['id']
+    assert response_film_star_trek.status == 200
 
-    # 2
-    assert r2.status == 200
-    assert len(r2.body) == 1
-    assert r2.body[0]['uuid'] == films[0]['id']
-
-    # 3
-    assert r3.status == 404
+    assert len(response_film_star_trek.body) == 1
+    film_star_trek_es = response_film_star_trek.body[0]
+    film_star_trek = films[2]
+    assert film_star_trek_es['title'] == film_star_trek.title
+    assert film_star_trek_es['uuid'] == film_star_trek.id
+    assert film_star_trek_es['imdb_rating'] == film_star_trek.imdb_rating
 
 
-@ pytest.mark.asyncio
-async def test_search_by_id(es_client, make_get_request, films):
-    requests = [
-        make_get_request('/film/08293f6d-90e4-467d-b2c3-ee85b5b0d326'),  # 1 Удачный тест
-        make_get_request('/film/00000000-0000-0000-0000-000000000000'),  # 2 Нет такого id
-        make_get_request('/film/1'),                                     # 3 Странный id
-    ]
+@pytest.mark.asyncio
+async def test_many_search(make_get_request, films):
+    film1, film2 = films[2], films[3]
+    response_films_star_trek = await make_get_request('/film/?query=' + urllib.parse.quote('Trek'))
 
-    responses = await asyncio.gather(*requests)
-    r1, r2, r3 = responses
+    assert response_films_star_trek.status == 200
+    assert len(response_films_star_trek.body) == 2
 
-    # 1
-    assert r1.status == 200
-    assert r1.body['title'] == 'Night Owl'
+    films_star_trek_es_ids = {f['uuid'] for f in response_films_star_trek.body}
+    assert film1.id in films_star_trek_es_ids
+    assert film2.id in films_star_trek_es_ids
 
-    # 2
-    assert r2.status == 404
 
-    # 3
-    assert r3.status == 404
+@pytest.mark.asyncio
+async def test_query_params(make_get_request, films):
+    response_films_star_trek = await make_get_request('/film/?query=' + urllib.parse.quote('Trek') + '&filter[genre]=67ae3870-b50d-4508-b2a6-ade667149ceb')
+    assert response_films_star_trek.status == 200
+    assert len(response_films_star_trek.body) == 1
+
+    response_films_star_trek = await make_get_request('/film/?query=' + urllib.parse.quote('Trek') + '&filter[genre]=80746937-2fc6-45d7-a3e6-a5854aa66092')
+    assert response_films_star_trek.status == 404
+
+    response_films_star_trek = await make_get_request('/film/?query=' + urllib.parse.quote('trek') + '&filter[genre]=1')
+    assert response_films_star_trek.status == 422
+
+    response_films_star_trek = await make_get_request('/film/?query=' + urllib.parse.quote('trek') + '&sort=S')
+    assert response_films_star_trek.status == 404
+
+    response_films_star_trek = await make_get_request('/film/?query=' + urllib.parse.quote('trek') + '&sort=-imdb_rating')
+    assert response_films_star_trek.status == 200
+    assert len(response_films_star_trek.body) == 2
+
+    assert response_films_star_trek.body[0]['uuid'] == films[3].id
+
+    response_films_star_trek = await make_get_request('/film/?query=' + urllib.parse.quote('Trek') + '&page[size]=1')
+    assert response_films_star_trek.status == 200
+    assert len(response_films_star_trek.body) == 1
+
+    response_films_star_trek = await make_get_request('/film/?query=' + urllib.parse.quote('Trek') + '&page[size]=1&page[number]=2')
+    assert response_films_star_trek.status == 200
+    assert len(response_films_star_trek.body) == 1
+
+    response_films_star_trek = await make_get_request('/film/?query=' + urllib.parse.quote('Trek') + '&page[size]=1&page[number]=3')
+    assert response_films_star_trek.status == 404
+    assert 'detail' in response_films_star_trek.body
+
+    response_films_star_trek = await make_get_request('/film/?query=' + urllib.parse.quote('Trek') + '&page[size]=-1')
+    assert response_films_star_trek.status == 422
+
+    response_films_star_trek = await make_get_request('/film/?query=' + urllib.parse.quote('Trek') + '&page[size]=1&page[number]=-1')
+    assert response_films_star_trek.status == 422
+
+
+@pytest.mark.asyncio
+async def test_all_search(make_get_request):
+    response_films_star_trek = await make_get_request('/film/?query=' + urllib.parse.quote('Star'))
+
+    assert response_films_star_trek.status == 200
+    assert len(response_films_star_trek.body) == 4
+
+
+@pytest.mark.asyncio
+async def test_search_not_found(make_get_request):
+    response_not_found = await make_get_request('/film/?query=NotFoundFilm&' + urllib.parse.quote(''))
+
+    assert response_not_found.status == 404
+    assert 'detail' in response_not_found.body
+
+
+@pytest.mark.asyncio
+async def test_detailed_info(make_get_request, films):
+    film_star_wars = films[1]
+    response_film_star_wars = await make_get_request(f'/film/{film_star_wars.id}')
+
+    assert response_film_star_wars.status == 200
+    film_star_wars_es = response_film_star_wars.body
+
+    assert film_star_wars_es['uuid'] == film_star_wars.id
+    assert film_star_wars_es['title'] == film_star_wars.title
+    assert film_star_wars_es['imdb_rating'] == film_star_wars.imdb_rating
+    assert film_star_wars_es['description'] == film_star_wars.description
+
+    assert len(film_star_wars_es['genre']) == len(film_star_wars.genres)
+    fg = {g.id: g for g in film_star_wars.genres}
+    for genre in film_star_wars_es['genre']:
+        assert genre['uuid'] in fg
+        assert genre['name'] == fg[genre['uuid']].name
+
+    assert len(film_star_wars_es['actors']) == len(film_star_wars.actors)
+    fa = {a.id: a for a in film_star_wars.actors}
+    for actor in film_star_wars_es['actors']:
+        assert actor['uuid'] in fa
+        assert actor['full_name'] == fa[actor['uuid']].name
+
+    assert len(film_star_wars_es['writers']) == len(film_star_wars.writers)
+    fw = {w.id: w for w in film_star_wars.writers}
+    for writer in film_star_wars_es['writers']:
+        assert writer['uuid'] in fw
+        assert writer['full_name'] == fw[writer['uuid']].name
+
+    assert len(film_star_wars_es['directors']) == len(film_star_wars.directors)
+    fd = {d.id: d for d in film_star_wars.directors}
+    for director in film_star_wars_es['directors']:
+        assert director['uuid'] in fd
+        assert director['full_name'] == fd[director['uuid']].name
+
+
+@pytest.mark.asyncio
+async def test_detailed_not_found(make_get_request):
+    response_not_found = await make_get_request('/film/6bcc7f85-9e5d-45a9-91ec-25903212c8b7')
+
+    assert response_not_found.status == 404
+    assert 'detail' in response_not_found.body
+
+    response_not_found = await make_get_request('/ film/-1?')
+    assert response_not_found.status == 404
+    assert 'detail' in response_not_found.body
+
+
+@pytest.mark.asyncio
+async def test_redis_cache(make_get_request, redis: Redis):
+    await redis.flushall()
+    assert not (await redis.keys("Film:query:*"))
+    response = await make_get_request('/film/?query=Trek')
+
+    assert response.status == 200
+    assert await redis.keys("Film:query:*")
